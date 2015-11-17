daa7c7d8472587c8a260940faa57f9ecda0b8649
Evgueni Ratchev <evgueni@Evguenis-MacBook-Pro.local>
2015-11-16 21:02:23 -0800

    redis config working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,10 @@ require 'sinatra/activerecord'
 
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  register Sinatra::ConfigFile
   register Sinatra::Health
 
   set :service_name, 'greetings'
+  config_file '../../config/*.erb'
+
 end
